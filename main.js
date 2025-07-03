f5c071a1f72b4476f6062091f0274278a34ce0d9
Nathn <nathan@tranchant.fr>
2025-07-03 16:10:22 +0200

    V1.0 : Reliable server build

--- main.js
+++ main.js
@@ -7,8 +7,7 @@ app.commandLine.appendSwitch('enable-experimental-web-platform-features');
 
 const client = new net.Socket();
 
-const pythonServer = spawn('python', [path.join(__dirname, '/serveur/server.py')], { // DEV
-// const pythonServer = spawn('python', [path.join(process.resourcesPath, 'serveur', 'server.py')], { // PROD
+const pythonServer = spawn('python', [path.join(process.resourcesPath, 'server', 'server.py')], {
   stdio: ['pipe', 'pipe', 'pipe']
 }).on('error', (err) => {
   console.error('Failed to start Python process:', err);
